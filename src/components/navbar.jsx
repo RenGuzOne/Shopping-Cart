5bea56f1f42a21fa97aee0bb2e57d4b78fdecfc6
Ren <rguzmanone@gmail.com>
2024-12-30 16:25:40 -0500

    Applied navbar links and cart

---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from './Navbar.module.css'
+
+function Navbar({ cartItemCount }) {
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.logo}>MY STORE </div>
+      <nav className={styles.navbar}>
+      <div className={styles.navLinks}>
+        <Link to="/"className={styles.link}>Home</Link>
+        <Link to="/shop" className={styles.link}>Shop</Link>
+        <div className={styles.cartInfo}>
+          <span> Cart: {cartItemCount}</span>
+          <Link to="/cart" className={styles.checkoutButton}>Checkout</Link>
+        </div>
+      </div>
+    </nav>
+    </div>
+  );
+}
+
+export default Navbar;
