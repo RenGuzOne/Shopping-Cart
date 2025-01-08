956f58200a8c6bca119cc70784f967b019743216
Ren <rguzmanone@gmail.com>
2025-01-08 15:12:25 -0500

    Applied and initiated testing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import App from './App.jsx'
 import Home from './pages/Home.jsx'
 import Shop from './pages/Shop.jsx'
 import Cart from './pages/Cart.jsx'
-
 import Clearance from './pages/Clearance.jsx'
 
 const router = createBrowserRouter([
